9ef6d132a46766ab1e13c7c04489f8a6de6dbc81
송준석 <rosaa9979@gmail.com>
2024-10-26 02:15:10 +0900

    정의실현부 개발 및 카드 타입 컨디션 여러개 지정 가능하도록 변경

--- CardArchive/Assets/TcgEngine/Scripts/Conditions/ConditionCardType.cs
+++ CardArchive/Assets/TcgEngine/Scripts/Conditions/ConditionCardType.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace TcgEngine
@@ -12,9 +13,9 @@ namespace TcgEngine
     public class ConditionCardType : ConditionData
     {
         [Header("Card is of type")]
-        public CardType has_type;
-        public ClubData has_club;
-        public TraitData has_trait;
+        public List<CardType> has_type;
+        public List<ClubData> has_club;
+        public List<TraitData> has_trait;
 
         public ConditionOperatorBool oper;
 
@@ -35,18 +36,26 @@ namespace TcgEngine
 
         public override bool IsTargetConditionMet(Game data, AbilityData ability, Card caster, CardData target)
         {
+            /*
             bool is_type = target.type == has_type || has_type == CardType.None;
             bool is_club = target.HasClub(has_club) || has_club == null;
             bool is_trait = target.HasTrait(has_trait) || has_trait == null;
+            //return (is_type && is_team && is_trait);
+            return (is_type && is_club && is_trait);
+            */
+
+            bool is_type = has_type.Contains(target.type) || has_type.Count == 0;
+            bool is_club = has_club.Any(club => target.HasClub(club)) || has_club.Count == 0;
+            bool is_trait = has_trait.Any(trait => target.HasTrait(trait)) || has_trait.Count == 0;
             //return (is_type && is_team && is_trait);
             return (is_type && is_club && is_trait);
         }
 
         private bool IsTrait(Card card)
         {
-            bool is_type = card.CardData.type == has_type || has_type == CardType.None;
-            bool is_club = card.HasClub(has_club) == has_club || has_club == null;
-            bool is_trait = card.HasTrait(has_trait) || has_trait == null;
+            bool is_type = has_type.Contains(card.CardData.type) || has_type.Count == 0;
+            bool is_club = has_club.Any(club => card.HasClub(club)) || has_club.Count == 0;
+            bool is_trait = has_trait.Any(trait => card.HasTrait(trait)) || has_trait.Count == 0;
             //return (is_type && is_team && is_trait);
             return (is_type && is_club && is_trait);
         }
