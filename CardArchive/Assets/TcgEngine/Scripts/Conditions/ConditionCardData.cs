bff76b8f39a4e0169dbf8f5a20ec44b5f7cbab31
송준석 <rosaa9979@gmail.com>
2024-10-28 03:41:06 +0900

    미드덱 완성

--- CardArchive/Assets/TcgEngine/Scripts/Conditions/ConditionCardData.cs
+++ CardArchive/Assets/TcgEngine/Scripts/Conditions/ConditionCardData.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace TcgEngine
@@ -12,13 +13,58 @@ namespace TcgEngine
     public class ConditionCardData : ConditionData
     {
         [Header("Card is")]
-        public CardData card_type;
+        public List<CardData> card_types;
 
         public ConditionOperatorBool oper;
 
         public override bool IsTargetConditionMet(Game data, AbilityData ability, Card caster, Card target)
         {
-            return CompareBool(target.card_id == card_type.id, oper);
+            /*
+            foreach(CardData card_type in card_types)
+                if (CompareBool(target.card_id == card_type.id, oper))
+                    return true;
+            return false;
+            */
+
+            bool exists = card_types.Any(card => card.id == target.card_id);
+
+            Debug.Log(exists);
+
+            // oper 값에 따라 조건을 검사
+            if (oper == ConditionOperatorBool.IsTrue && exists)
+            {
+                return true;  // oper이 True이고, card_id가 리스트에 존재하는 경우
+            }
+            else if (oper == ConditionOperatorBool.IsFalse && !exists)
+            {
+                return true;  // oper이 False이고, card_id가 리스트에 없는 경우
+            }
+
+            return false;  // 조건을 만족하지 않는 경우
+        }
+
+        public override bool IsTargetConditionMet(Game data, AbilityData ability, Card caster, CardData target)
+        {
+            /*
+            foreach(CardData card_type in card_types)
+                if (CompareBool(target.card_id == card_type.id, oper))
+                    return true;
+            return false;
+            */
+
+            bool exists = card_types.Any(card => card.id == target.id);
+
+            // oper 값에 따라 조건을 검사
+            if (oper == ConditionOperatorBool.IsTrue && exists)
+            {
+                return true;  // oper이 True이고, card_id가 리스트에 존재하는 경우
+            }
+            else if (oper == ConditionOperatorBool.IsFalse && !exists)
+            {
+                return true;  // oper이 False이고, card_id가 리스트에 없는 경우
+            }
+
+            return false;  // 조건을 만족하지 않는 경우
         }
 
         public override bool IsTargetConditionMet(Game data, AbilityData ability, Card caster, Player target)
