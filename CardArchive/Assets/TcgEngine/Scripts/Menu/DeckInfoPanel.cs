1d88b5bb3648532a0fa1239e105651a470f3190d
pucat <rosaa9979@gmail.com>
2025-09-09 18:27:12 +0900

    아로나 미리보기 적용

--- CardArchive/Assets/TcgEngine/Scripts/Menu/DeckInfoPanel.cs
+++ CardArchive/Assets/TcgEngine/Scripts/Menu/DeckInfoPanel.cs
@@ -18,6 +18,10 @@ namespace TcgEngine.UI
     {
         [Header("Arona Ability")]
         [SerializeField] AronaSelection arona_selection;
+        [SerializeField] UIPanel ui_panel;
+        [SerializeField] CardUI card_ui;
+        public float hover_delay = 0.5f;
+        private float preview_timer = 0.0f;
 
         [Header("Mana Curve")]
         [SerializeField] ManaCurve mana_curve;
@@ -42,6 +46,24 @@ namespace TcgEngine.UI
         protected override void Update()
         {
             base.Update();
+
+            IconButton icon = IconButton.GetFocus("arona");
+            CardData icon_data = icon != null ? CardData.Get(icon.value) : null;
+
+            float delay = icon_data != null ? hover_delay : 0.0f;
+
+            bool should_show_preview = icon_data != null;
+
+            if (should_show_preview)
+                preview_timer += Time.deltaTime;
+            else
+                preview_timer = 0.0f;
+
+            bool should_show = should_show_preview && preview_timer >= delay;
+            ui_panel.SetVisible(should_show);
+
+            if (should_show)
+                card_ui.SetCard(icon_data, VariantData.GetDefault());
         }
 
         public override void Show(bool instance = false)
