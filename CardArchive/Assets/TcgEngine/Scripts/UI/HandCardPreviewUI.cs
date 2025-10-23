bf06e56d1f8541242fb2d55fdaf6e6225e003ea9
송준석 <rosaa9979@gmail.com>
2025-10-24 02:02:25 +0900

    status list 임시 출력 완료

--- CardArchive/Assets/TcgEngine/Scripts/UI/HandCardPreviewUI.cs
+++ CardArchive/Assets/TcgEngine/Scripts/UI/HandCardPreviewUI.cs
@@ -6,6 +6,7 @@ using TcgEngine.Client;
 using TcgEngine;
 using System.Diagnostics.SymbolStore;
 using DG.Tweening;
+using System;
 
 namespace TcgEngine.UI
 {
@@ -17,6 +18,7 @@ namespace TcgEngine.UI
     {
         public UIPanel ui_panel;
         public CardUI card_ui;
+        public GameObject status_ui;
         private HandCard current_focus;
 
         public RectTransform ui_row;
@@ -25,11 +27,16 @@ namespace TcgEngine.UI
 
         private Vector2 ui_start_pos;
         private Vector2 status_start_pos;
+        private float offset;
+
+        [Header("Default Setting")]
+        public ConditionWideAreaRange default_wide_area_range;
 
         void Start()
         {
             ui_start_pos = ui_row.anchoredPosition;
-            status_start_pos = status_row.transform.position;
+            status_start_pos = status_row.anchoredPosition;
+            offset = Math.Abs(ui_row.gameObject.transform.position.x - status_row.gameObject.transform.position.x);
         }
 
         void Update()
@@ -49,8 +56,57 @@ namespace TcgEngine.UI
                     SetCard();
                 }
 
-                Vector3 position = new Vector3(current_focus.gameObject.transform.position.x, card_ui.gameObject.transform.position.y, card_ui.gameObject.transform.position.z);
-                card_ui.gameObject.transform.position = position;
+                Vector3 ui_position = new Vector3(current_focus.gameObject.transform.position.x, card_ui.gameObject.transform.position.y, card_ui.gameObject.transform.position.z);
+                card_ui.gameObject.transform.position = ui_position;
+
+                Vector3 status_position = new Vector3(current_focus.gameObject.transform.position.x + offset, status_ui.gameObject.transform.position.y, status_ui.gameObject.transform.position.z);
+                status_ui.gameObject.transform.position = status_position;
+
+                Card pcard = hcard.GetCard();
+
+                int index = 0;
+                foreach (AbilityData ability in pcard.GetAbilities())
+                {
+                    if (index < status_lines.Length)
+                    {
+                        if (ability.condition_wide_range != default_wide_area_range)
+                        //Dont display default ability (GetAbilitiesDesc does that already)
+                        //if (!pcard.CardData.HasAbility(ability) && !string.IsNullOrWhiteSpace(ability.desc))
+                        {
+                            status_lines[index].SetLine(pcard.CardData, ability.condition_wide_range.thumnail);
+                            index++;
+                        }
+                    }
+                }
+
+                foreach (AbilityData ability in pcard.GetAbilities())
+                {
+                    if (index < status_lines.Length)
+                    {
+                        if (!string.IsNullOrWhiteSpace(ability.desc))
+                        //Dont display default ability (GetAbilitiesDesc does that already)
+                        //if (!pcard.CardData.HasAbility(ability) && !string.IsNullOrWhiteSpace(ability.desc))
+                        {
+                            status_lines[index].SetLine(pcard.CardData, ability);
+                            index++;
+                        }
+                    }
+                }
+
+                //Status
+                foreach (CardStatus status in pcard.GetAllStatus())
+                {
+                    if (index < status_lines.Length)
+                    {
+                        StatusData istatus = StatusData.Get(status.type);
+                        if (istatus != null && !string.IsNullOrWhiteSpace(istatus.desc))
+                        {
+                            int ival = Mathf.Max(status.value, Mathf.CeilToInt(status.duration / 2f));
+                            status_lines[index].SetLine(istatus, ival);
+                            index++;
+                        }
+                    }
+                }
             }
 
             else
